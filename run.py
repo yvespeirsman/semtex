13e78adf9be6a4a4b5f07dd3e3c5c41b10316f43
yvespeirsman <yves.peirsman@gmail.com>
2013-06-26 23:15:19 +0200

    further extended run.py with Frog and ElasticSearch functions

--- run.py
+++ run.py
@@ -1,19 +1,60 @@
 import glob
 import HTMLDocument
+import FrogText
+import LanguageGuesser
 import re
+import json
+import requests
+import os
 
+def normalize():
+    files = glob.glob('articles/html-new/*')
+    for f in files:
+        fName = f.split('/')[-1]
+        oName = re.sub('.html','.txt',fName)
+        oName = "articles/txt-new/" + oName
+    
+        html = HTMLDocument.HTMLDocument(f)
+        txt = html.normalize()
 
-files = glob.glob('articles/html-new/*')
-for f in files:
-    fName = f.split('/')[-1]
-    oName = re.sub('.html','.txt',fName)
-    oName = "articles/txt-new/" + oName
+        o = open(oName,'w')
+        o.write(txt)
+        o.close()
+        print oName
 
-    html = HTMLDocument.HTMLDocument(f)
-    txt = html.normalize()
+def makeJSON():
+    files = glob.glob('articles/frog/*')
+    languageGuesser = LanguageGuesser.LanguageGuesser()
 
-    o = open(oName,'w')
-    o.write(txt)
-    o.close()
-    print oName
+    for f in files:
+        d = {}
+        t = FrogText.FrogText(f)
 
+        lang = languageGuesser.guessLanguage(t.lemmas)
+
+        fn = f.split("/")[-1].split(".")[0]
+        print fn, lang
+        d = {"lemmas": t.lemmas,
+             "text": t.tokens,
+             "language": lang}
+
+        fo = "articles/json/" + fn + ".json"
+        with open(fo,'w') as outFile:
+            json.dump(d,outFile)
+        
+def addToElastic():
+    r = requests.get('http://localhost:9200')
+
+    files = glob.glob('articles/json/*')
+    id = 0
+    for f in files:
+        id += 1
+        with open(f,'r') as i:
+            j = json.load(i)
+         
+            cm = 'curl -XPUT http://localhost:9200/staatsblad/article/' + str(id) + ' -d \'' + json.dumps(j) + '\''
+            os.system(cm)
+
+addToElastic()
+        
+ 
