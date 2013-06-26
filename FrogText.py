546acfbf57c2cf36384ffaa8911d7698c88bb10d
yvespeirsman <yves.peirsman@gmail.com>
2013-06-26 22:27:35 +0200

    renamed FrogFile to FrogText and added token output

--- FrogText.py
+++ FrogText.py
@@ -4,8 +4,10 @@ class FrogText:
 
     def __init__(self,fileName):
         self.lemmas = []
+        self.tokens = []
         with open(fileName) as i:
             for line in i:
+                line = line.split("\t")
                 if len(line) > 2:
-                    self.lemmas.append(line.split("\t")[2])
- 
+                    self.lemmas.append(line[2])
+                    self.tokens.append(line[1])
